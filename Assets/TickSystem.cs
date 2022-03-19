be2270f6f45cf40726de7f316623205e5c2fcfbc
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2022-03-19 19:12:19 +0300

    Added TickSystem, UI buttons, sprites

---
+++ Assets/TickSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickSystem : MonoBehaviour
+{
+    public delegate void TickDelegate();
+
+    public event TickDelegate TickEvent;
+
+    public void Tick()
+    {
+        TickEvent.Invoke();
+    }
+}
---
+++ Assets/TickableObjectInterface.cs
@@ -0,0 +1,4 @@
+interface ITickableObject
+{
+    void ActionOnTick();
+}
